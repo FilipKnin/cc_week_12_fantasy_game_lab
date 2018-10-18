1f5d01deb23a273a92afdc97753180b6747333d7
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-18 12:21:23 +0100

    Player class created

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -1,4 +1,18 @@
 package players;
 
-public class Player {
+import java.util.ArrayList;
+
+public abstract class Player {
+
+    private int hp;
+    private int strength;
+    private ArrayList<Tools> tools;
+
+    public Player(int hp, int strength) {
+        this.hp = hp;
+        this.strength = strength;
+        this.tools = new ArrayList<>();
+    }
+
+
 }
