047c36669240316f093edf6d004bfec2770b32c3
Laura Nagy <ngylaura@gmail.com>
2018-10-18 12:27:06 +0100

    adds and tests treasure and boss attributes in Room

--- src/main/java/rooms/Room.java
+++ src/main/java/rooms/Room.java
@@ -1,4 +1,28 @@
 package rooms;
 
 public class Room {
+
+    private int treasure;
+    private String boss;
+
+    public Room() {
+        this.treasure = 0;
+        this.boss = null;
+    }
+
+    public int getTreasure() {
+        return this.treasure;
+    }
+
+    public void setTreasure(int treasure) {
+        this.treasure = treasure;
+    }
+
+    public String getBoss() {
+        return this.boss;
+    }
+
+    public void setBoss(String boss) {
+        this.boss = boss;
+    }
 }
